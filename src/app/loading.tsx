298d71e13855cc7c560604483f6d8f08746d70c1
Phuong Dang <powoftech@gmail.com>
2024-11-20 19:17:52 +0700

    feat: add @radix-ui/react-tooltip dependency; refactor Loading component to remove progress state; implement sleep utility function; enhance Tailwind config with sidebar colors

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,34 +2,9 @@
 
 import { Icon } from "@/app/_images/icon";
 import { LogoLight } from "@/app/_images/logo";
-import { Progress } from "@/components/ui/progress";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 export default function Loading() {
-  const [progress, setProgress] = useState(0);
-  const [isIncreasing, setIsIncreasing] = useState(true);
-
-  useEffect(() => {
-    const STEP = 20;
-
-    const timer = setInterval(() => {
-      setProgress((prevProgress) => {
-        if (prevProgress >= 100 - STEP / 2) {
-          setIsIncreasing(false);
-          return prevProgress - STEP;
-        }
-        if (prevProgress <= STEP / 2) {
-          setIsIncreasing(true);
-          return prevProgress + STEP;
-        }
-        return isIncreasing ? prevProgress + STEP : prevProgress - STEP;
-      });
-    }, 100);
-
-    return () => clearInterval(timer);
-  }, [isIncreasing]);
-
   return (
     <div className="flex h-screen items-center justify-center text-center">
       <div className="flex flex-col items-center justify-center gap-8 sm:gap-12">
@@ -47,7 +22,6 @@ export default function Loading() {
           className="block dark:invert sm:hidden"
           priority
         />
-        <Progress value={progress} className="w-48 sm:w-128" />
       </div>
     </div>
   );
