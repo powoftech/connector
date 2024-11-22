0b384526b03b885bf5fc43b5b0d81215314d39c1
Phuong Dang <powoftech@gmail.com>
2024-11-22 11:30:13 +0700

    feat: implement CSV import functionality for country data and update GET endpoint response

--- src/app/api/data/location/route.ts
+++ src/app/api/data/location/route.ts
@@ -1,82 +1,82 @@
 import { NextResponse } from "next/server";
 
-// import prisma from "@/lib/prisma";
-// import csv from "csv-parser";
-// import fs from "fs";
+import prisma from "@/lib/prisma";
+import csv from "csv-parser";
+import fs from "fs";
 
-// async function importCSV(filePath: string) {
-//   try {
-//     const data: { name: string; code: string; cities: string[] }[] = [];
+async function importCSV(filePath: string) {
+  try {
+    const data: { name: string; code: string; cities: string[] }[] = [];
 
-//     await new Promise((resolve, reject) => {
-//       fs.createReadStream(filePath)
-//         .pipe(csv())
-//         .on("data", (row) => {
-//           const countryName = row["country"];
-//           const countryCode = row["iso3"];
-//           const cityName = row["combined_city"];
+    await new Promise((resolve, reject) => {
+      fs.createReadStream(filePath)
+        .pipe(csv())
+        .on("data", (row) => {
+          const countryName = row["country"];
+          const countryCode = row["iso3"];
+          const cityName = row["combined_city"];
 
-//           const countryData = data.find((c) => c.code === countryCode);
-//           if (countryData) {
-//             countryData.cities.push(cityName);
-//           } else {
-//             data.push({
-//               name: countryName,
-//               code: countryCode,
-//               cities: [cityName],
-//             });
-//           }
-//         })
-//         .on("end", resolve)
-//         .on("error", reject);
-//     });
+          const countryData = data.find((c) => c.code === countryCode);
+          if (countryData) {
+            countryData.cities.push(cityName);
+          } else {
+            data.push({
+              name: countryName,
+              code: countryCode,
+              cities: [cityName],
+            });
+          }
+        })
+        .on("end", resolve)
+        .on("error", reject);
+    });
 
-//     // console.log("Data length: ", data.length);
-//     // let citiesLength = 0;
-//     // data.forEach(async (country) => {
-//     //   console.log(
-//     //     `Importing country: ${country.name} (${country.code}) - Cities: ${country.cities.length}`
-//     //   );
-//     //   citiesLength += country.cities.length;
-//     // });
-//     // console.log("Cities length: ", citiesLength);
+    // console.log("Data length: ", data.length);
+    // let citiesLength = 0;
+    // data.forEach(async (country) => {
+    //   console.log(
+    //     `Importing country: ${country.name} (${country.code}) - Cities: ${country.cities.length}`
+    //   );
+    //   citiesLength += country.cities.length;
+    // });
+    // console.log("Cities length: ", citiesLength);
 
-//     data.forEach(async (country) => {
-//       console.log(`Importing country: ${country.name} (${country.code})`);
+    data.forEach(async (country) => {
+      console.log(`Importing country: ${country.name} (${country.code})`);
 
-//       await prisma.country.create({
-//         data: {
-//           name: country.name,
-//           code: country.code,
-//           cities: {
-//             create: country.cities.map((city) => ({ name: city })),
-//           },
-//         },
-//       });
-//     });
+      await prisma.country.create({
+        data: {
+          name: country.name,
+          code: country.code,
+          cities: {
+            create: country.cities.map((city) => ({ name: city })),
+          },
+        },
+      });
+    });
 
-//     return true;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-
-// export async function GET() {
-//   try {
-//     await importCSV("database/worldcities_edited.csv");
-//     return NextResponse.json({ message: "Success" }, { status: 200 });
-//   } catch (error) {
-//     console.error("Error importing data:", error);
-//     return NextResponse.json(
-//       { message: "Error", details: error },
-//       { status: 500 }
-//     );
-//   }
-// }
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
 
 export async function GET() {
-  return NextResponse.json(
-    { message: "This route is disabled" },
-    { status: 200 }
-  );
+  try {
+    await importCSV("database/worldcities_edited.csv");
+    return NextResponse.json({ message: "Success" }, { status: 200 });
+  } catch (error) {
+    console.error("Error importing data:", error);
+    return NextResponse.json(
+      { message: "Error", details: error },
+      { status: 500 }
+    );
+  }
 }
+
+// export async function GET() {
+//   return NextResponse.json(
+//     { message: "This route is disabled" },
+//     { status: 200 }
+//   );
+// }
